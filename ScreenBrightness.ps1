cbb33feb0ed3d10805f7b99013f6ac23091bf0f3
PiotrDudzinski <piotr_dudzinski@outlook.com>
2019-03-31 17:29:18 +0200

    Restore previous brightness after change

--- ScreenBrightness.ps1
+++ ScreenBrightness.ps1
@@ -1,21 +1,25 @@
-﻿$code = @'
+$code = @'
     [DllImport("user32.dll")]
      public static extern IntPtr GetForegroundWindow();
 '@
 Add-Type $code -Name Utils -Namespace Win32
-while(1){
+$Brightness = (Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness).CurrentBrightness
+while(1)
+{
     $hwnd = [Win32.Utils]::GetForegroundWindow()
     $focus = (Get-Process | 
         Where-Object { $_.mainWindowHandle -eq $hwnd } | 
         Select-Object processName, MainWindowTItle, MainWindowHandle).ProcessName
-    sleep -Milliseconds 200
+
     if($focus -eq 'chrome')
     {
         (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,0)
     }
     else
     {
-        (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,70)
+        (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,$Brightness)
+        $Brightness = (Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness).CurrentBrightness
     }
+    sleep -Milliseconds 200
     Write-Host $focus
 }
