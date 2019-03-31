98d524c7ac2ce7b0943eef7899d6b3c48a3b5217
PiotrDudzinski <piotr_dudzinski@outlook.com>
2019-03-31 17:02:00 +0200

    Add files via upload

---
+++ ScreenBrightness.ps1
@@ -0,0 +1,24 @@
+﻿$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+Add-Type $code -Name Utils -Namespace Win32
+while(1){
+    $hwnd = [Win32.Utils]::GetForegroundWindow()
+    $focus = (Get-Process | 
+        Where-Object { $_.mainWindowHandle -eq $hwnd } | 
+        Select-Object processName, MainWindowTItle, MainWindowHandle).ProcessName
+    sleep -Milliseconds 200
+    if($focus -eq 'chrome')
+    {
+        (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,0)
+    }
+    else
+    {
+        (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,70)
+    }
+    Write-Host $focus
+}
+
+#zobacz co zrobiłem, jak przeglądam memy to mi się ekran sam ściemna
+#jak robię coś innego to się podjaśnia 
